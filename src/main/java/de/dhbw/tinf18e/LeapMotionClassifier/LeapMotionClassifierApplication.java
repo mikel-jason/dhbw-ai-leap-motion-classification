b1a5b61d6bea3d6774096b33e20839626e731793
Mikel Muennekhoff <inf18207@lehre.dhbw-stuttgart.de>
2021-04-12 09:08:48 +0200

    Initial commit
    https://start.spring.io + https://gitignore.io

---
+++ src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/LeapMotionClassifierApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.tinf18e.LeapMotionClassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapMotionClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapMotionClassifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dhbw/tinf18e/LeapMotionClassifier/LeapMotionClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.tinf18e.LeapMotionClassifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeapMotionClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
