2551eff02fcaaa3be88897f3465c0a9129188149
Niels Heidbrink <niels.heidbrink@mway.io>
2021-05-03 17:25:31 +0200

    I tried something

--- src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/io/LeapDataReader.java
+++ src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/io/LeapDataReader.java
@@ -1,8 +1,13 @@
 package de.dhbw.tinf18e.LeapMotionClassifier.io;
 
-import com.opencsv.bean.CsvToBeanBuilder;
 import de.dhbw.tinf18e.LeapMotionClassifier.leap.LeapRecord;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -13,15 +18,20 @@ import java.util.List;
 @Service
 public class LeapDataReader {
 
-    public List<LeapRecord> readLeapData(Path path) throws IOException {
+    public List<LeapRecord> readLeapData(Path path) throws Exception {
+        try {
+            CsvMapper mapper = new CsvMapper();
+            mapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-        Reader reader = Files.newBufferedReader(path);
-        List<LeapRecord> list = new CsvToBeanBuilder<LeapRecord>(reader)
-                .withType(LeapRecord.class)
-                .build()
-                .parse();
+            CsvSchema schema = mapper.schemaFor(LeapRecord.class).withHeader().withColumnSeparator(',');
 
-        reader.close();
-        return list;
+            ObjectReader reader = mapper.readerFor(LeapRecord.class).with(schema);
+
+            return reader.<LeapRecord>readValues(Files.newBufferedReader(path)).readAll();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Exception("Exception in CSV Parser!");
+        }
     }
 }
--- src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/leap/LeapRecord.java
+++ src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/leap/LeapRecord.java
@@ -1,6 +1,7 @@
 package de.dhbw.tinf18e.LeapMotionClassifier.leap;
 
-import com.opencsv.bean.CsvBindByName;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 import lombok.Setter;
 
@@ -8,23 +9,12 @@ import lombok.Setter;
  * Entity representing a LEAP data record as given
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LeapRecord {
-
-    @CsvBindByName(column = "Palm_Position_X")
     private double palmPositionX;
-
-    @CsvBindByName(column = "Palm_Position_Y")
     private double palmPositionY;
-
-    @CsvBindByName(column = "Palm_Position_Z")
     private double palmPositionZ;
-
-    @CsvBindByName(column = "Valid")
     private short valid;
-
-    @CsvBindByName(column = "Thumb")
     private double thumb;
-
-    @CsvBindByName(column = "Spread")
     private double spread;
 }
