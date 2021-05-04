4083599e99fecf21566a0ae13d009bc37e6d0b78
Mikel Muennekhoff <inf18207@lehre.dhbw-stuttgart.de>
2021-05-04 08:15:02 +0200

    Replace openCSV with jackson @ csv writer

--- src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/io/LeapFrameWriter.java
+++ src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/io/LeapFrameWriter.java
@@ -1,31 +1,35 @@
 package de.dhbw.tinf18e.LeapMotionClassifier.io;
 
-import com.opencsv.CSVWriter;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import de.dhbw.tinf18e.LeapMotionClassifier.ai.Difficulty;
 import de.dhbw.tinf18e.LeapMotionClassifier.ai.FrequencyLevel;
 import de.dhbw.tinf18e.LeapMotionClassifier.ai.Motion;
 import de.dhbw.tinf18e.LeapMotionClassifier.detector.EdgeDetector;
 import de.dhbw.tinf18e.LeapMotionClassifier.leap.LeapFrame;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LeapFrameWriter {
 
     /**
      * Inner class for selecting data to be written to csv
-     *
+     * <p>
      * Note: Columns will be ordered alphabetically, not corresponding to their order in here
      */
+    @Getter
+    @Setter
     private class WrittenFrame {
 
         private int frameNum;
@@ -50,13 +54,14 @@ public class LeapFrameWriter {
 
     }
 
-    public void write(List<LeapFrame> frames, Path path) throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
-        BufferedWriter writer = Files.newBufferedWriter(path);
-        StatefulBeanToCsv<WrittenFrame> beanToCsv = new StatefulBeanToCsvBuilder<WrittenFrame>(writer)
-                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
-                .build();
-        beanToCsv.write(frames.stream().map(WrittenFrame::new));
-        writer.close();
-    }
+    public void write(List<LeapFrame> frames, Path path) throws IOException {
 
+        CsvMapper mapper = new CsvMapper();
+        mapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+
+        CsvSchema schema = mapper.schemaFor(WrittenFrame.class).withHeader().withColumnSeparator(',');
+        ObjectWriter writer = mapper.writerFor(WrittenFrame.class).with(schema);
+
+        writer.writeValues(path.toFile()).writeAll(frames.stream().map(WrittenFrame::new).collect(Collectors.toList()));
+    }
 }
