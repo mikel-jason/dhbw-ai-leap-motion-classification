1754cfa3244987f299c2e47f3870cbdb0f254d51
Mikel Muennekhoff <inf18207@lehre.dhbw-stuttgart.de>
2021-05-04 07:51:55 +0200

    Fix csv import

--- src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/io/LeapDataReader.java
+++ src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/io/LeapDataReader.java
@@ -1,26 +1,31 @@
 package de.dhbw.tinf18e.LeapMotionClassifier.io;
 
-import de.dhbw.tinf18e.LeapMotionClassifier.leap.LeapRecord;
-import org.springframework.stereotype.Service;
-
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import de.dhbw.tinf18e.LeapMotionClassifier.leap.LeapRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class LeapDataReader {
 
-    public List<LeapRecord> readLeapData(Path path) throws Exception {
+    private static final Logger LOGGER = LogManager.getLogger(LeapDataReader.class);
+
+    public List<LeapRecord> readLeapData(Path path) {
         try {
             CsvMapper mapper = new CsvMapper();
+            mapper.enable(CsvParser.Feature.IGNORE_TRAILING_UNMAPPABLE);
             mapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
             mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
@@ -30,8 +35,8 @@ public class LeapDataReader {
 
             return reader.<LeapRecord>readValues(Files.newBufferedReader(path)).readAll();
         } catch (IOException e) {
-            e.printStackTrace();
-            throw new Exception("Exception in CSV Parser!");
+            LOGGER.error("Exception in CSV Parser!", e);
+            return new ArrayList<>();
         }
     }
 }
--- src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/leap/LeapRecord.java
+++ src/main/java/de/dhbw/tinf18e/LeapMotionClassifier/leap/LeapRecord.java
@@ -1,9 +1,8 @@
 package de.dhbw.tinf18e.LeapMotionClassifier.leap;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.Setter;
 
 /**
  * Entity representing a LEAP data record as given
@@ -11,10 +10,22 @@ import lombok.Setter;
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LeapRecord {
+
+    @JsonProperty("Palm_Position_X")
     private double palmPositionX;
+
+    @JsonProperty("Palm_Position_Y")
     private double palmPositionY;
+
+    @JsonProperty("Palm_Position_Z")
     private double palmPositionZ;
+
+    @JsonProperty("Valid")
     private short valid;
+
+    @JsonProperty("Thumb")
     private double thumb;
+
+    @JsonProperty("Spread")
     private double spread;
 }
